743a1b3ea3adba9938d4c6bfe368ad440a163803
atakan <atakanarici97@gmail.com>
2025-10-22 00:09:00 +0300

    22.10.2025

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ana Sayfa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&family=Jockey+One&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&family=Sriracha&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+        
+    <div class="navbar">
+        <div class="navbar-img">
+            <a href="#"><img src="images/logo2.png" alt="Ana Sayfa">Ana Sayfa</a>
+        </div>
+        <div class="navbar-links">
+            <a href="about.php">Hakkımızda</a>
+            <a href="services.php">Hizmetler</a>
+            <a href="contact.php">İletişim</a>
+            <a href="login.php">Giriş Yap</a>
+    </div>
+    
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Giriş Yap</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<style>
+    body{
+        background: linear-gradient(to bottom right, #A1C4FD, #C2A2F8);
+        width: 100%;
+        height: 100vh; 
+    }
+
+</style>
+<body>
+    
+    <div class="container">
+        <div class="col-4">
+            
+        </div>
+
+    </div>
+
+</body>
+</html>
